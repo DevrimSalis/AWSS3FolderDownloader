53dfdb57722668278c20034cdcc06f753e4e159b
Devrim SALIŞ <devrimsalis06@outlook.com>
2023-05-29 18:56:19 +0300

    Add project files.

---
+++ AWSS3FolderDownloader/Program.cs
@@ -0,0 +1,51 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+
+        string awsAccessKeyId = "YOUR_ACCESS_KEY";
+        string awsSecretAccessKey = "YOUR_SECRET_KEY";
+        string bucketName = "YOUR_BUCKET_NAME";
+        string folderKey = "YOUR_FOLDER_KEY";
+        string localPath = "YOUR_LOCAL_PATH";
+
+        var config = new AmazonS3Config
+        {
+            RegionEndpoint = RegionEndpoint.GetBySystemName("eu-west-1"),
+            ForcePathStyle = true,
+            SignatureVersion = "2",
+            ServiceURL = "https://s3.eu-west-1.amazonaws.com"
+        };
+
+        using var client = new AmazonS3Client(awsAccessKeyId, awsSecretAccessKey, config);
+
+        var listObjectsRequest = new ListObjectsRequest
+        {
+            BucketName = bucketName,
+            Prefix = folderKey
+        };
+
+        var listObjectsResponse = await client.ListObjectsAsync(listObjectsRequest);
+
+        foreach (var s3Object in listObjectsResponse.S3Objects)
+        {
+            if (!s3Object.Key.EndsWith("/"))
+            {
+                var getObjectRequest = new GetObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = s3Object.Key
+                };
+
+                var getObjectResponse = await client.GetObjectAsync(getObjectRequest);
+
+                var filePath = Path.Combine(localPath, s3Object.Key);
+                var directoryPath = Path.GetDirectoryName(filePath);
+
+                Directory.CreateDirectory(directoryPath);
+                await using var fileStream = File.Create(filePath);
+                await getObjectResponse.ResponseStream.CopyToAsync(fileStream);
+            }
+        }
+
+        Console.WriteLine("Download Success");
